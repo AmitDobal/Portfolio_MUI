29e46734f3609e4d8e1a553e5fa07be80e609b1c
Amit Dobal <2019pte5188@mnit.ac.in>
2023-11-29 00:16:06 +0530

    Nav bar added

---
+++ frontend/src/Components/Navbar/NavAppBar/NavAppBar.js
@@ -0,0 +1,44 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import React from "react";
+
+const NavAppBar = ({ navItems, handleDrawerToggle }) => {
+  return (
+    <AppBar component={"nav"} position="fixed" color="secondary">
+      <Toolbar>
+        <IconButton
+          onClick={handleDrawerToggle}
+          color="inherit"
+          aria-label="open drawer"
+          sx={{ mr: 2, display: { sm: "none" } }}>
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          variant="h6"
+          sx={{
+            flexGrow: 1,
+            textAlign: { xs: "right", sm: "left" },
+          }}>
+          Amit
+        </Typography>
+
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          {navItems.map((item) => (
+            <Button key={item} sx={{ color: "#fff" }}>
+              {item}
+            </Button>
+          ))}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavAppBar;
